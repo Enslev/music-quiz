dc89d82ab633fa3e8d6ad958e0bab74cfc08842c
Jeppe Enslev <jeppe@enslev.dk>
2023-04-20 09:27:43 +0200

    Use env for creds

--- server/src/mongoose/index.ts
+++ server/src/mongoose/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const init = async () => {
-    const mongoUser = 'musicquiz-user';
-    const mongoPassword = '4VLjtWnU8XRfDz4q';
+    const mongoUser = process.env.MONGO_USER;
+    const mongoPassword = process.env.MONGO_PASSWORD;
     await mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@music-quiz.ptregv5.mongodb.net/?retryWrites=true&w=majority`);
 };
